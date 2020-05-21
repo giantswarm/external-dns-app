0489612fd73b4efaa9bb21dddf012555af0e8ef2
Ross Fairbanks <ross@rossfairbanks.com>
2020-05-21 09:26:18 +0200

    Use helmclient v1.0.0

--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -88,11 +88,8 @@ func init() {
 
 	{
 		c := helmclient.Config{
-			Logger:     l,
-			K8sClient:  k8sClients.K8sClient(),
-			RestConfig: k8sClients.RESTConfig(),
-
-			TillerNamespace: "giantswarm",
+			Logger:    l,
+			K8sClient: k8sClients,
 		}
 		helmClient, err = helmclient.New(c)
 		if err != nil {
