ac6e59eb9b0c6b5187c5fd75ef13e604ffb009ad
Matías Charrière <matias@giantswarm.io>
2025-11-25 19:16:08 +0100

    Add sync script  (#384)
    * Add sync script with VPA and NetPol patches
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * add CRDs patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * add deployment patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * add helper patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * add values patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * add readme patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Enable patches and add diffs
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * refresh PR template
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * remove readme patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * remove unused cmd in sync
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    ---------
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

---
+++ sync/patches/network-policies/patch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+repo_dir=$(git rev-parse --show-toplevel) ; readonly repo_dir
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) ; readonly script_dir
+
+cd "${repo_dir}"
+
+readonly script_dir_rel=".${script_dir#"${repo_dir}"}"
+
+set -x
+cp "${script_dir_rel}/networkpolicy.yaml" ./helm/external-dns-app/templates/np.yaml
+
+{ set +x; } 2>/dev/null
