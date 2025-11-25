a738f2580554c2616bed269a2c2c1e99289a5d84
Matias Charriere <matias@giantswarm.io>
2025-11-25 11:12:39 +0100

    add CRDs patch
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

---
+++ sync/patches/crds/patch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+repo_dir=$(git rev-parse --show-toplevel) ; readonly repo_dir
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) ; readonly script_dir
+
+cd "${repo_dir}"
+
+readonly script_dir_rel=".${script_dir#"${repo_dir}"}"
+
+set -x
+rm -rf ./helm/external-dns-app/crds
+
+{ set +x; } 2>/dev/null
+
+cp -R "${script_dir_rel}/charts" ./helm/external-dns-app/ 
+
+set -x
+
+{ set +x; } 2>/dev/null
